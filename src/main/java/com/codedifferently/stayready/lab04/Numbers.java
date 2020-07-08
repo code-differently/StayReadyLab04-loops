c9052c25fa9b5b39fcc291bfae6a2fcb3849fa3a
CourtneeL <48140227+CourtneeL@users.noreply.github.com>
2020-07-08 10:07:47 -0400

    'master'

--- src/main/java/com/codedifferently/stayready/lab04/Numbers.java
+++ src/main/java/com/codedifferently/stayready/lab04/Numbers.java
@@ -1,51 +1,64 @@
 package com.codedifferently.stayready.lab04;
 
 
-public class Numbers 
-{
+public class Numbers {
 
-    public String oneToTen()
-    {
+    public String oneToTen(){
     	String output ="oneToTen()\n"
     				   +"*** Output ***";
-        for(int x=1; x<=10; x++)
-        {
+    	for(int x=1; x<=10; x++){
     		output += "\n"+x;
     	}
         return output;
     }
 
-
-    public String oddNumbers()
-    {
+    public String oddNumbers(){
     	String output = "oddNumbers()\n"
     					+"*** Output ***";
-        for(int x =1; x < 20 ; x+=2)
-        {
+    	for(int x =1; x < 20 ; x+=2){
     		output += "\n"+x;
     	}
     		
         return output;
     }
 
-    public String squares()
-    {
+    public String squares(){
+        String str = "squares()\n*** Output ***\n";
+        for(int i = 1; i <= 10; i++){
+            if(i != 10){
+            str += (int)Math.pow(i, 2) + "\n";
+            }
+            else{
+                str += (int)Math.pow(i, 2); 
+            }
+
+        }
+        return str;
+    }
+
+    public String random4(){
+        
         return "";
     }
 
-    public String random4()
-    {
-        return "";
+    public String even(int n){
+        String output = "evenNumbers()\n"
+    					+"*** Output ***";
+    	for(int i = 2; i < 20 ; i += 2){
+    		output += "\n"+x;
+    	}
+        return output;
     }
 
-    public String even(int n)
-    {
-        return "";
-    }
-
-    public String powers(int n)
-    {
-        return "";
+    public String powers(int n){
+        String str = "powers()\n"
+                      + "*** Output ***\n";
+        for(int i = 2; i < 300; i++){
+            if(i % 2 == 0){
+                str += (int) Math.pow(i, 2);
+            }
+        }
+        return str;
     }
 
 }
--- src/test/java/com/codedifferently/stayready/lab04/NumbersTest.java
+++ src/test/java/com/codedifferently/stayready/lab04/NumbersTest.java
@@ -3,12 +3,10 @@ package com.codedifferently.stayready.lab04;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class NumbersTest 
-{
+public class NumbersTest {
 
     @Test
-    public void oneToTenTest()
-    {
+    public void oneToTenTest(){
         //: Given
         Numbers numbers = new Numbers();
         String expected = "oneToTen()\n*** Output ***\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
@@ -21,8 +19,7 @@ public class NumbersTest
     }
 
     @Test
-    public void oddNumbersTest()
-    {
+    public void oddNumbersTest(){
         //: Given
     	Numbers numbers = new Numbers();
     	String expected ="oddNumbers()\n*** Output ***\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
@@ -35,8 +32,20 @@ public class NumbersTest
     }
 
     @Test
-    public void squaresTest()
-    {
+    public void squaresTest(){
+        //: Given
+        Numbers numbers = new Numbers();
+        String expected = "squares()\n*** Output ***\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
+
+        //: When
+        String actual = numbers.squares();
+
+        //: Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void random4Test(){
         //: Given
 
         //: When
@@ -45,33 +54,29 @@ public class NumbersTest
     }
 
     @Test
-    public void random4Test()
-    {
+    public void evenTest(){
         //: Given
-
+    	Numbers numbers = new Numbers();
+    	String expected ="evenNumbers()\n*** Output ***\n2\n4\n6\n8\n10\n12\n14\n16\n18";
         //: When
 
+    	String actual = numbers.evenNumbers();
         //: Then
+    	
+    	Assert.assertEquals("The two strings are equal",expected,actual);
     }
 
     @Test
-    public void evenTest()
-    {
+    public void powersTest(){
         //: Given
+    	Numbers numbers = new Numbers();
+    	String expected ="powers()\n*** Output ***\n2\n4\n8\n16\n32\n64\n128\n256";
 
         //: When
+        String actual = numbers.powers();
 
         //: Then
-    }
-
-    @Test
-    public void powersTest()
-    {
-        //: Given
-
-        //: When
-
-        //: Then
+        Assert.assertEquals(expected, actual);
     }
 
 }
