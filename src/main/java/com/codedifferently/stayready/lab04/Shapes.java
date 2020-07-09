2ec14bd0a4233ce86450cb630a17d8f952ec68f3
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-08 21:08:00 -0400

    Lab04 finished and ready for review babeyyyyyy

--- src/main/java/com/codedifferently/stayready/lab04/Shapes.java
+++ src/main/java/com/codedifferently/stayready/lab04/Shapes.java
@@ -3,15 +3,44 @@ package com.codedifferently.stayready.lab04;
 
 public class Shapes {
 
+    public static String getRow(int width) {
+        String result = "";
+        for(int i = 1; i <= width; i++){
+            result += "*";
+        }
+        return result;
+    }
+
     public String triangle(){
-        return "";
+        String result = "triangle()\n" +
+                        "*** Output ***\n";
+        for(int row = 1; row <= 5; row++){
+            result += getRow(row) + "\n";
+        }
+        return result;
     }
 
     public String tableSquare(){
-        return "";
+        String result = "tableSquare()\n" +
+                        "*** Output ***\n";
+        for(int col = 1; col <= 4; col++){
+            for(int row = 1; row <= 4; row++){
+                result += String.format("%3d |", col * row);
+            }
+            result += "\n";
+        }
+        return result;
     }
 
-    public String tableSquares(int n){
-        return "";
+    public String tableSquares(int tableSize){
+        String result = "tableSquare()\n" +
+                        "*** Output ***\n";
+        for(int col = 1; col <= tableSize; col++){
+            for(int row = 1; row <= tableSize; row++){
+                result += String.format("%3d |", col * row);
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
