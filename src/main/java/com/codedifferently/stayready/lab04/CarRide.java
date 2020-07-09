3bc2dd723d26e7fedd7e1038e91618a035b0bf77
Giovane Barnes <gjbarnes19@students.desu.edu@Giovanes-MacBook-Pro.local>
2020-07-08 21:12:13 -0400

    Lab 04 Loops completed with Francisco

--- src/main/java/com/codedifferently/stayready/lab04/CarRide.java
+++ src/main/java/com/codedifferently/stayready/lab04/CarRide.java
@@ -1,9 +1,12 @@
 package com.codedifferently.stayready.lab04;
 
-public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+public class CarRide {
+    public Boolean areWeThereYet(String verdict) {
+        if (verdict.equals("Yes")) {
+            return true;
+        }
+        return false;
     }
 
 }
