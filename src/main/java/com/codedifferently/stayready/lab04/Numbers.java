6b592489a2593b3803aa69d09681f93537a1f24d
madlynjoa1 <maddlynjoa1@icloud.com>
2020-07-08 22:38:02 -0400

    Let's get that belt

--- src/main/java/com/codedifferently/stayready/lab04/Numbers.java
+++ src/main/java/com/codedifferently/stayready/lab04/Numbers.java
@@ -1,6 +1,8 @@
 package com.codedifferently.stayready.lab04;
 
 
+import java.util.Random;
+
 public class Numbers {
 
     public String oneToTen(){
@@ -23,19 +25,48 @@ public class Numbers {
     }
 
     public String squares(){
-        return "";
+        String output= "squares()\n"
+        +"*** Output ***";
+        for(int x=1; x<=10; x++){
+            output+= "\n"+(int)Math.pow(x, 2);
+        }
+        return output;
     }
 
     public String random4(){
-        return "";
+        Random random= new Random();
+        String newRandom= "random4()\n"
+        +"*** Output ***";
+        //get 4 random numbers
+        for(int i =1; i<=4;i++){
+            newRandom+="\n"+random.nextInt(10);
+        }
+        return newRandom;
+
     }
 
     public String even(int n){
-        return "";
+        String output = "even()\n"
+        +"*** Output ***";
+        //intitiation; verification; step
+        for(int i = 2; i < n; i+=2){
+            output += "\n" + i; 
+        }
+        return output;
     }
-
+//ASK TARIQ EHY THEY NEED AN INITIATION INSIDE LOOP
     public String powers(int n){
-        return "";
+        String output = "powers()\n"
+                +"*** Output ***";
+        Integer number = 1;
+        //for(int i=1 ; i <= n; i++);{
+        int i = 1;
+        while(i<=n){
+            number =(int)Math.pow(2, i);
+            output +="\n"+ number;
+            i++;
+        }    
+        return output;
     }
 
 }
