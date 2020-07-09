96a8fbd2d990559088c93860c5ff327caed8431c
Lissette Garcia <lissettegarcia@Lissettes-MacBook-Air.local>
2020-07-09 10:20:22 -0500

    Completed lab04 with Dominic

--- src/test/java/com/codedifferently/stayready/lab04/CarRideTest.java
+++ src/test/java/com/codedifferently/stayready/lab04/CarRideTest.java
@@ -1,9 +1,49 @@
 package com.codedifferently.stayready.lab04;
 
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CarRideTest {
+    @Test
+    public void answerYesTest(){
+        // Given
+        CarRide carRide = new CarRide();
+        String input = "Yes";
+        boolean expected = true;
 
+        // When
+        boolean actual = carRide.answerEval(input);
 
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void answerNoTest(){
+        // Given
+        CarRide carRide = new CarRide();
+        String input = "No";
+        boolean expected = false;
+
+        // When
+        boolean actual = carRide.answerEval(input);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void answerAnythingTest(){
+        // Given
+        CarRide carRide = new CarRide();
+        String input = "East Side";
+        boolean expected = false;
+
+        // When
+        boolean actual = carRide.answerEval(input);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
 }
+
