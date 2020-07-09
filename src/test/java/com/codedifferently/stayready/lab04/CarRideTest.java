02f808ad36efb7e45f1c431902a914380aab74c1
mercelisvaughan <mercelis.vaughan51@gmail.com>
2020-07-09 12:11:47 -0400

    Ahmad and Mercelis' lab 4 completed with testing.

--- src/test/java/com/codedifferently/stayready/lab04/CarRideTest.java
+++ src/test/java/com/codedifferently/stayready/lab04/CarRideTest.java
@@ -1,9 +1,20 @@
 package com.codedifferently.stayready.lab04;
 
 
+import org.junit.Assert;
 import org.junit.Test;
+
+
 
 public class CarRideTest {
 
-
+    @Test
+    public void areWeThereYetTestNo(){
+        // Given
+        CarRide carRide = new CarRide();
+        // When
+        Boolean actual = carRide.areWeThereYet("yooo");
+        // Then
+        Assert.assertFalse(actual);
+    }
 }
