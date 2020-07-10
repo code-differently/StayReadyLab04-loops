113139bef9509b678f24ad947f4aca775f394ee4
Makhya Wilridge <makhyaw@makhyas-air.myfiosgateway.com>
2020-07-09 20:07:45 -0400

    done

--- src/main/java/com/codedifferently/stayready/lab04/CarRide.java
+++ src/main/java/com/codedifferently/stayready/lab04/CarRide.java
@@ -1,9 +1,43 @@
 package com.codedifferently.stayready.lab04;
+import java.util.Scanner;
 
 public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+    public String str = "Are we there yet?"; //init string
+    Scanner sc = new Scanner(System.in); // set up of scanner object for user input
+
+    //areWeThereYet returns a string...debated if it should just return void. no arguments accepted.
+    public String areWeThereYet(){
+        //prints a message out to the user
+        System.out.println(str);
+        //begin loop, this while the user input is not yes;while isYes != true.
+
+        while(isYes(sc.nextLine()) != true){
+            //it'll keep asking are we there yet
+            System.out.println(str);
+            //it'll keep asking for user input
+            String reAsk = sc.nextLine();
+            //it'll keep evaluating the user input as true or false.
+            isYes(reAsk);
+        }
+        //finally returns str
+        return str;
+    }
+
+    public boolean isYes(String input){ //helper function
+        String neccesaryCon = "Yes";    //condition to return true
+
+        if(neccesaryCon.equals(input)){
+            str = "Good"; // public string is reassigned to "Good"
+            return true;
+        }
+        return false; //this second return works like an else statement...
+    }
+
+    public static void main(String[] args){
+        CarRide carRide = new CarRide();
+        //Printing out return value from areWeThereYet();
+        System.out.println(carRide.areWeThereYet());
     }
 
 }
--- src/test/java/com/codedifferently/stayready/lab04/NumbersTest.java
+++ src/test/java/com/codedifferently/stayready/lab04/NumbersTest.java
@@ -34,37 +34,47 @@ public class NumbersTest {
     @Test
     public void squaresTest(){
         //: Given
-
+        Numbers numbers = new Numbers();
+        String expected ="squares()\n*** Output ***\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
         //: When
-
+        String actual = numbers.squares();
         //: Then
+        Assert.assertEquals("The two strings are equal",expected,actual);
     }
 
     @Test
     public void random4Test(){
         //: Given
-
+        Numbers numbers = new Numbers();
+        String numStr = numbers.random4();
+        String[] strArr = numStr.split("\n");
+        int expected = 6;
         //: When
-
+        int actual = strArr.length;
         //: Then
+        Assert.assertEquals("The two ints are equal",expected,actual);
     }
 
     @Test
     public void evenTest(){
         //: Given
-
+        Numbers numbers = new Numbers();
+        String expected ="even(int n)\n*** Output ***\n2\n4\n6\n8\n10\n12\n14\n16\n18";
         //: When
-
+        String actual = numbers.even(18);
         //: Then
+        Assert.assertEquals("The two strings are equal",expected,actual);
     }
 
     @Test
     public void powersTest(){
         //: Given
-
+        Numbers numbers = new Numbers();
+        String expected ="powers(8)\n*** Output ***\n2\n4\n8\n16\n32\n64\n128\n256";
         //: When
-
+        String actual = numbers.powers(8);
         //: Then
+        Assert.assertEquals("The two strings are equal",expected,actual);
     }
 
 }
